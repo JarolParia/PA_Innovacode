e73c849e11f1020209677ee5d2b13633f1af0220
Jarol Paria <jarolparia10@gmail.com>
2026-04-19 19:17:13 -0500

    Creacion de carpeta de proyecto de mongo con grafios y inicializacion de app.py junto con creacion de archivo de requerimientos

---
+++ ProyectoMongo/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+def main():
+    st.title("Proyecto MongoDB")
+    st.write("Bienvenido a la aplicación de MongoDB. Aquí puedes realizar diversas operaciones con tu base de datos.")
+
+
+
+if __name__ == "__main__":
+    main()
